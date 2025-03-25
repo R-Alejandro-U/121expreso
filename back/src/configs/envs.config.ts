b1122bc70080f40997a5067044f37ae9cad7f513
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-25 16:23:11 -0300

    envs de la base de datos y mejoras en el archivo main de la aplicación.

--- back/src/configs/envs.config.ts
+++ back/src/configs/envs.config.ts
@@ -3,27 +3,27 @@ import 'dotenv/config';
 const data: NodeJS.ProcessEnv = process.env;
 export const PORT: number = data['PORT'] ? parseInt(data['PORT'], 10) : 3000;
 export const CONECTION_STREAM: string | undefined = data['CONECTION_STREAM'];
-const DB_TYPE: any = data["DB_TYPE"] ?? 'postgres';
-const DB_HOST: string | undefined = data["DB_HOST"];
-const DB_PORT: number | undefined = data["DB_PORT"]
-  ? parseInt(data["DB_PORT"], 10)
+const DB_TYPE: any = data['DB_TYPE'] ?? 'postgres';
+const DB_HOST: string | undefined = data['DB_HOST'];
+const DB_PORT: number | undefined = data['DB_PORT']
+  ? parseInt(data['DB_PORT'], 10)
   : 5432;
-const DB_USERNAME: string | undefined = data["DB_USERNAME"];
-const DB_PASSWORD: string | undefined = data["DB_PASSWORD"];
-const DB_NAME: string | undefined = data["DB_NAME"];
-const DB_SYNCHRONIZE: boolean | undefined = data["DB_SYNCHRONIZE"]
-  ? data["DB_SYNCHRONIZE"] === 'true'
+const DB_USERNAME: string | undefined = data['DB_USERNAME'];
+const DB_PASSWORD: string | undefined = data['DB_PASSWORD'];
+const DB_NAME: string | undefined = data['DB_NAME'];
+const DB_SYNCHRONIZE: boolean | undefined = data['DB_SYNCHRONIZE']
+  ? data['DB_SYNCHRONIZE'] === 'true'
   : true;
-const DB_LOGGING: boolean | undefined = data["DB_LOGGING"]
-  ? data["DB_LOGGING"] === 'true'
+const DB_LOGGING: boolean | undefined = data['DB_LOGGING']
+  ? data['DB_LOGGING'] === 'true'
   : true;
-const DB_ENTITIES: string[] | undefined = data["DB_ENTITIES"]
-  ? !data["DB_ENTITIES"].includes(', .')
-    ? data["DB_ENTITIES"].split(',').map((path) => path.trim())
-    : [data["DB_ENTITIES"].trim()]
+const DB_ENTITIES: string[] | undefined = data['DB_ENTITIES']
+  ? !data['DB_ENTITIES'].includes(', .')
+    ? data['DB_ENTITIES'].split(',').map((path) => path.trim())
+    : [data['DB_ENTITIES'].trim()]
   : ['./dist/**/*.entity{.ts, .js}'];
-const DB_DROPSCHEMA: boolean | undefined = data["DB_DROP_SCHEMA"]
-  ? data["DB_DROP_SCHEMA"] === 'true'
+const DB_DROPSCHEMA: boolean | undefined = data['DB_DROP_SCHEMA']
+  ? data['DB_DROP_SCHEMA'] === 'true'
   : false;
 export const dbConfig = {
   type: DB_TYPE,
--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,20 @@
+import { AppDataSource } from './configs/database.config';
 import { PORT } from './configs/envs.config';
 import server from './server';
 import 'reflect-metadata';
-server.listen(PORT, () =>
-  console.log(`Server is listening in the port ${PORT}.`),
-);
+
+AppDataSource.initialize()
+  .then((): void => {
+    console.log('connection sucessfull.');
+    server.listen(PORT, () =>
+      console.log(`Server is listening in the port ${PORT}.`),
+    );
+  })
+  .catch((err): void => {
+    throw new Error(
+      `Error connecting to the database: ${err['message'] ?? err}`,
+    );
+  })
+  .finally((): void => {
+    console.log('The server is on.');
+  });
