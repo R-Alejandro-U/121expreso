af233e817167c8df7170cb398c5e83a927339f84
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-25 15:12:00 -0300

    arreglo rapido

--- back/src/configs/envs.config.ts
+++ back/src/configs/envs.config.ts
@@ -1,3 +1,4 @@
 import "dotenv/config"
 const data: NodeJS.ProcessEnv = process.env;
 export const PORT: number = data["PORT"] ? parseInt(data["PORT"], 10) : 3000;
+export const CONECTION_STREAM: string | undefined = data["CONECTION_STREAM"];
--- back/src/radio/radio.service.ts
+++ back/src/radio/radio.service.ts
@@ -1,14 +1,16 @@
 import axios from "axios"
 import { Readable } from "node:stream";
+import { CONECTION_STREAM } from "../configs/envs.config";
 
 export const RadioService = {
     getUrlRadioLife: async (): Promise<Readable> => {
         try {
-            const { data } = await axios("https://sonic.host-live.com/8006/stream" , {responseType: "stream"});
+            if(!CONECTION_STREAM) throw new Error('Falta la url de conexión para el stream.')
+            const { data } = await axios(CONECTION_STREAM , {responseType: "stream"});
             return data;
         } catch (error) {
             const err: string = error instanceof Error ? error.message : 'Error desconocido.';
-            throw new Error(`Hubo un error al pedir la información al stream. Error: ${err}.`);
+            throw new Error(`Hubo un error al pedir la información al stream. Error: ${err}`);
         };
     }
 };
