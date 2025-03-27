579f4f0d5b6ec5711da29e4119142f295bbc4603
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-26 22:30:21 -0300

    Entidad corregida

--- back/src/comments/Comment.entity.ts
+++ back/src/comments/Comment.entity.ts
@@ -1,13 +1,12 @@
 import { Min } from "class-validator";
 import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { typeorm } from "../configs/database.config";
 import { User } from "../users/User.entity";
 
 @Entity({name: "comments"})
 export class Comment {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
-    @Column({type: 'text', length: 500, nullable: false})
+    @Column({type: 'varchar', length: 500, nullable: false})
     @Min(4, {message: 'El mínimo es de 4 caracteres'})
     content!: string;
     @CreateDateColumn()
@@ -17,5 +16,3 @@ export class Comment {
     @ManyToOne(() => User, (user) => user.comments)
     user!: User;
 };
-
-export const userModel = typeorm.getRepository(Comment);
