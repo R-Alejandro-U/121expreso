1567df0062c3b45d07ebd1a1013ce712878c0539
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-26 22:30:05 -0300

    endpoints de auht terminados y funcionales

---
+++ back/src/Auth/auth.service.ts
@@ -0,0 +1,43 @@
+import * as bcrypt from 'bcrypt';
+import { SALT, SECRET_WORD } from '../configs/envs.config';
+import Jwt from 'jsonwebtoken'
+import { LoginResponse, UserLoginDTO, UserRegisterDTO } from './auth.dto';
+import { userModel } from '../configs/database.config';
+import { User } from '../users/User.entity';
+
+const hashPassword = async (password: string): Promise<string> => {
+    try {
+        const salt: string = await bcrypt.genSalt(SALT);
+        return await bcrypt.hash(password, salt);
+    } catch (error) {
+        const err: string = error instanceof Error ? error.message : 'Error desconocido.'
+        throw new Error(`Hubo un error en el hasheo de la contraseña. Error: ${err}`);            
+    };
+};
+
+export const authService = {
+    register: async (user: Omit<UserRegisterDTO, 'passwordConfirmation'>): Promise<string> => {
+        try {
+            const password: string = await hashPassword(user.password);
+            await userModel.save({ ...user, password, isDeleted: false});
+            return 'Te has registrado con éxito.';
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        } catch (error: any) {
+            console.log(('Hubo un error al registrar al usuario: ' + (error['detail'] || error['message'] || 'Error desconocido.')));
+            throw 'Hubo un error al registrar al usuario: ' + (error['detail'] || error['message'] || 'Error desconocido.');
+        };
+    },
+    login: async ({ email, password }: UserLoginDTO): Promise<LoginResponse> => {
+        try {
+            const user: User | null = await userModel.findOneBy({ email });
+            if (!user || !(await bcrypt.compare(password, user.password))) throw new Error('Contraseña o email incorrectos.');
+            if(user.isDeleted) throw new Error('Contraseña o email incorrectos.'); 
+            const { id, name, isAdmin } = user;
+            const token: string = Jwt.sign({ id, name }, SECRET_WORD, { expiresIn: '1d' });
+            return { user: { id, name, isAdmin }, token };
+        } catch (error) {
+            const err: string = error instanceof Error ? error.message : 'Hubo un error desconocido.';
+            throw err;
+        };
+    },
+};
