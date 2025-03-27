2c1500cc25385e64a8a37941aad476bc92affff4
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-27 00:44:41 -0300

    Entidad modificada para tener usuarios eliminados

--- back/src/users/User.entity.ts
+++ back/src/users/User.entity.ts
@@ -1,22 +1,24 @@
 import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
-import { Comment } from "../comments/Comment.entity";
-import { typeorm } from "../configs/database.config";
 import { Min } from "class-validator";
+import { Comment } from "../comments/Comment.entity";
 
 @Entity({name: "users"})
 export class User {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
     @Column({type: 'varchar', length: 100, nullable: false})
-    @Min(4, {message: 'El mínimo es de 4 caracteres'})
+    @Min(4, {message: 'El mínimo es de 4 caracteres'}) 
     name!: string;
     @Column({type: 'varchar', nullable: false, unique: true})
     email!: string;
     @Column({type: 'text', nullable: false})
     password!: string;
+    @Column({type: 'boolean', default: false})
+    isAdmin!: boolean;
+    @Column({type: 'boolean', default: false})
+    isDeleted!: boolean;
     @CreateDateColumn()
     CreateAcount?: Date;
     @OneToMany(() => Comment, (comment) => comment.user, {nullable: true})
     comments?: Comment;
 };
-export const userModel = typeorm.getRepository(User);
