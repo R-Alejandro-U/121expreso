89684a94b28a07f1b26abe1f722af9bd2061f426
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-03-28 03:33:23 -0300

    Dtos de comments

---
+++ back/src/comments/comment.dto.ts
@@ -0,0 +1,21 @@
+import { IsNotEmpty, IsString, Length } from "class-validator";
+import { IPages } from "../utils/globalDTOs/pages.dto";
+
+export class CommentDTO {
+    id?: string; 
+    user?: string; 
+    content?: string; 
+    CreateComment?: Date;
+}
+
+export class GetCommentsResponse {
+    comments!: CommentDTO[];
+    pagination_info!: IPages;
+}
+
+export class NewCommentDTO {
+    @IsNotEmpty({message: 'No puede estar vacio el comentario.'})
+    @IsString({message: 'Debe ser una cadena de texto.'})
+    @Length(4, 500, {message: 'El minimo de caracteres es de 4 y unmaximode 500.'})
+    comment!: string;
+}
