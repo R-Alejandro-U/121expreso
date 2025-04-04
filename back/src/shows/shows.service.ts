6eefa2a9376a254fb21af161ca4a5f3fa12dd70b
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-04-03 21:59:16 -0300

    cambios enlos headers para que render no explote

--- back/src/shows/shows.service.ts
+++ back/src/shows/shows.service.ts
@@ -6,7 +6,12 @@ import { IProgramLite } from "./Shows.dto";
 export const showsService = {
     getAllShows: async (): Promise<IProgramLite[]> => {
         try {
-            const { data } = await axios('https://api.mixcloud.com/121expreso/cloudcasts/');
+            const { data } = await axios('https://api.mixcloud.com/121expreso/cloudcasts/', {
+                headers: {
+                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+                    'Accept': 'application/json',
+                },
+            });
             if(!data['data'].length) throw new Error('Sin shows previos.');
             return data['data'].map((show: any) => ({name: show.name, url: show.url, image: show['pictures']['640wx640h'], duration: Number((show['audio_length'] / 3600).toFixed(2))}));
         } catch (error) {
