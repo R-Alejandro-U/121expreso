af35786d2d5eb25b1ba6c99116c1f827f1bd1a33
Ignacia22 <mfernandezpolanco@gmail.com>
2025-04-14 22:45:29 -0400

    se realizo la pagina de cafecito

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/comments': { // Asegúrate de que sea /comments, no /api
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
