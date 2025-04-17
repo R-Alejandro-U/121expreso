d0ddb2c58efaeae5039262417a55c527366795c8
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-04-16 23:45:01 -0300

    formulario

--- front/src/components/Forms/register/RegisterForm.tsx
+++ front/src/components/Forms/register/RegisterForm.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './style/register.module.css';
+import { IRegisterFormProps } from './RegisterForm.interface';
 
-export const RegisterForm: React.FC = () => {
+export const RegisterForm: React.FC<IRegisterFormProps> = ({data: { name, email, password, passwordConfirmation }, change, submit, errors}) => {
     return (
         <main>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={submit}>
                 <fieldset className={styles.border}>
                     <div>
-                    <legend className={styles.title}>Registro</legend>
+                        <legend className={styles.title}>Registro</legend>
                     </div>
                     <div>
                         <div className={styles.inputs}>
@@ -16,9 +17,12 @@ export const RegisterForm: React.FC = () => {
                                 id="name"
                                 type="text"
                                 name="name"
+                                value={name}
+                                onChange={change}
                                 placeholder="Gustavo Cerati"
                                 
                             />
+                            {errors?.name && <p>{errors?.name}</p>}
                         </div>
                         <div className={styles.inputs}>
                             <label htmlFor="email">Email</label>
@@ -26,9 +30,12 @@ export const RegisterForm: React.FC = () => {
                                 id="email"
                                 type="email"
                                 name="email"
+                                value={email}
+                                onChange={change}
                                 placeholder="Torpeman.Gus@gmail.com"
                                 
                             />
+                            {errors?.email && <p>{errors?.email}</p>}
                         </div>
                     </div>
                     <div>
@@ -38,18 +45,24 @@ export const RegisterForm: React.FC = () => {
                                 id="password"
                                 type="password"
                                 name="password"
+                                value={password}
+                                onChange={change}
                                 placeholder="Mer3cEs-l0-qUe-sueña5"
                                 
                             />
+                            {errors?.password && <p>{errors?.password}</p>}
                         </div>
                         <div className={styles.inputs}>
-                            <label htmlFor="confirm">Confirmar contraseña</label>
+                            <label htmlFor="passwordConfirmation">Confirmar contraseña</label>
                             <input 
-                                id="confirm"
+                                id="passwordConfirmation"
                                 type="password"
-                                name="confirm"
+                                name="passwordConfirmation"
+                                value={passwordConfirmation}
+                                onChange={change}
                                 placeholder="Mer3cEs-l0-qUe-sueña5"
                             />
+                            {errors?.passwordConfirmation && <p>{errors?.passwordConfirmation}</p>}
                         </div>
                     </div>
                     <div className={styles.buttons}>
