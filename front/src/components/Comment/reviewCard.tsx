bfde5dde805ccc0f01b0094731d754e720fadb15
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-04-25 15:07:26 -0300

    Modificada para recibir las propiedades

--- front/src/components/Comment/reviewCard.tsx
+++ front/src/components/Comment/reviewCard.tsx
@@ -1,10 +1,10 @@
-import { ReviewCardProps } from "../../interfaces/IReviewProps";
-import styles from './Reseñas.module.css';
+import { data } from '../../interfaces/IReviewProps';
+import styles from '../Forms/comments/styles/Reseñas.module.css';
 
-export const ReviewCard: React.FC<ReviewCardProps> = ({ text, author, date }) => (
+export const ReviewCard: React.FC<data> = ({data: { comment, CreateComment, user }}) => (
   <div className={styles.reviewCard}>
-    <p>{text}</p>
-    <p className={styles.reviewAuthor}>{author}</p>
-    <p className={styles.reviewDate}>{date}</p>
+    <p>{comment}</p>
+    <p className={styles.reviewAuthor}>{user}</p>
+    <p className={styles.reviewDate}>{CreateComment.toISOString().split('T')[0]}</p>
   </div>
 );
