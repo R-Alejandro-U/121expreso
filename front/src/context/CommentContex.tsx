29fc1be7e6e3e66857e1b5cec31f26172ee37a9c
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-04-25 15:08:04 -0300

    Funcionalidad en el contexto para que llame a los comentarios

--- front/src/context/CommentContex.tsx
+++ front/src/context/CommentContex.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
-import { createContext } from "react";
-export const CommentsContex: React.Context<any> = createContext<any>({
+import { createContext, useCallback, useState } from "react";
+import { comment, Comments, GetComments, GetCommentsRes } from "./interface/Comment.interface";
+
+export interface ICommentContext {
+    postReview: (comment: string, token: string) => Promise<string>,
+    getComments: () => Promise<GetComments[]> ,
+    reviews: GetComments[] | undefined
+}
+
+export const CommentsContext: React.Context<ICommentContext> = createContext<ICommentContext>({
     postReview: async () => ({} as string),
+    getComments: async () => ({} as GetComments[]),
+    reviews: []
 });
-interface comment {
-    message: string
-};
+
 export const ReviewsProvider = ({children}: {children: React.ReactNode}) => {
+    const [ reviews, setReviews ] = useState<GetComments[] | undefined>();
     const postReview = async (comment: string, token: string) => {
         try {
             const { data } = await axios.post<comment>('https://one21expreso.onrender.com/comment', {comment}, {
@@ -22,12 +31,24 @@ export const ReviewsProvider = ({children}: {children: React.ReactNode}) => {
             throw response.data.message;
         };
     };
+    const getComments = useCallback(async (): Promise<GetComments[]> => {
+        try {
+            const { data } = await axios<GetCommentsRes>('https://one21expreso.onrender.com/comment');
+            const comments: GetComments[] = data.data.comments.map((comment: Comments) => ({...comment, CreateComment: new Date(comment.CreateComment)}));
+            setReviews(comments);
+            return comments;
+        } catch ( { response }: any ) {
+            throw response.data;
+        };
+    }, [setReviews]);
     const value = {
-        postReview
+        postReview,
+        getComments,
+        reviews
     };
     return (
-        <CommentsContex.Provider value={value}> 
+        <CommentsContext.Provider value={value}> 
             {children} 
-        </CommentsContex.Provider>
+        </CommentsContext.Provider>
     );
 };
