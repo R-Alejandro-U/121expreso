112b4ca283b60f20c316dcd33d2f0fc6dd9b0677
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-05-04 01:07:25 -0300

    k

--- back/api/index.ts
+++ back/api/index.ts
@@ -21,11 +21,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse): 
     console.log('Database connection established.');
     await Seeder();
     console.log('Database seeded successfully.');
-    console.log(process.env['NODE_ENV']);
-    app.listen()
   } catch (err: any) {
     console.error('Database connection failed:', err);
     res.status(500).json({ error: 'Database connection failed' + err['message'] || err });
     return;
   };
+  return app(req, res)
 };
