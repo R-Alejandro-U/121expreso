c843c2a41bb258ca0a3992998c0f475cf99fd431
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-05-04 01:56:56 -0300

    liarwzfbliawugvliu

--- back/api/index.ts
+++ back/api/index.ts
@@ -12,7 +12,7 @@ const app: Express = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use('/api', routes);
 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Servidor funcionando correctamente' });
