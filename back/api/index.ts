da33a4c3c9b8d4841719203ca9175fafaeb771bd
R-Alejandro-U <alejandro.urdaneta2314@gmail.com>
2025-05-07 19:36:01 -0300

    luioykutjnyrbt

--- back/api/index.ts
+++ back/api/index.ts
@@ -3,27 +3,31 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import express, { Express } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import routes from '../src/Routes/index.routes';
 import { typeorm } from '../src/configs/database.config';
 import { Seeder } from '../src/Seeder/Seeder';
-
+import  route_users from '../src/Routes/users.routes'
+import  route_comment from '../src/Routes/comment.routes'
+import  route_auth from '../src/Routes/auth.routes'
+import  route_radio from '../src/Routes/radio.routes'
+import  route_shows from '../src/Routes/shows.routes'
 const app: Express = express();
-
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-app.use('/api', routes);
-
+app.use('/api/users', route_users);
+app.use('/api/auth', route_auth);
+app.use('/api/comment', route_comment);
+app.use('/api/radio', route_radio);
+app.use('/api/shows', route_shows);
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Servidor funcionando correctamente' });
 });
-
 export default async function handler(req: VercelRequest, res: VercelResponse): Promise<void> {
   try {
     if (!typeorm.isInitialized) {
       await typeorm.initialize();
       console.log('Database connection established.');
-    }
+    };
     await Seeder();
     console.log('Database seeded successfully.');
   } catch (err: any) {
