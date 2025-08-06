301c039e8340f81d12b7ff4747a3b79f5493682c
R-Alejandro-U <alejandro.urdaneta2314gmail.com>
2025-08-06 18:32:51 -0300

    etsrhterw

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,7 +1,7 @@
-import { PORT } from './configs/envs.config';
 import server from './server';
 import 'reflect-metadata';
-
+import 'dotenv/config'
+const PORT: number | undefined = process.env['PORT'] ? +process.env['PORT'] : 0;
 server.listen(PORT, () =>
   console.log(`Server is listening in the port ${PORT}.`),
 );
